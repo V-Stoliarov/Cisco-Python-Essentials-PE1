d0c6b9cd26f5aa2d161fb2c7648a780ac938a2fd
DankDragoness <201341750+V-Stoliarov@users.noreply.github.com>
2025-03-22 08:56:54 +0200

    Update list_duplicate_remover.py

--- list_duplicate_remover.py
+++ list_duplicate_remover.py
@@ -1,14 +1,23 @@
+# This is a primitive program to remove duplicates from current list
+# without a usage of list comprehension or dict.fromkeys()
+my_list = []
 
-my_list = [1, 1, 1, 1, 2, 2, 2, 4, 4, 4, 1, 1, 4, 2, 6, 6, 2, 9, 9, 9]
+# Prompting a user to fill the list with elements he wants:
+a = int(input("Enter the amount of elements you want to add to list: "))
+for x in range(a):
+    my_list.append(input("Enter the element want to add to list: "))\
+
 i = 0
 while i < len(my_list) :
     j = i+1
-    temp_val = my_list[i]
+    temp_val = my_list[i] # setting a value
     while j < len(my_list):
         if temp_val == my_list[j]:
             del my_list[j]
             continue
         else: j+=1
     i+=1;
+# the efficiency must be really bad for large lists
+
 print("The list with unique elements only:")
 print(my_list)
