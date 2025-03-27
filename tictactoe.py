2d2007b7a25e80b16ebaecd26fa32ed54515411c
DankDragoness <201341750+V-Stoliarov@users.noreply.github.com>
2025-03-27 20:14:19 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,23 +1,41 @@
 from random import randrange
 
-hor_line = "+-------+-------+-------+"
-ver_lines ="|       |       |       |"
-div = "  |  "
+hor_line = "+---------"
+ver_lines ="|         "
 end_of_game = False
-Nboard =[
-        ["1","2","3"],
-        ["4","X","6"],
-        ["7","8","9"]
-]
 
+
+def generate_board():
+    global n
+    global Nboard
+    while True: # loop for input validation
+        try:
+            n = int(input("Enter an odd number equal to or higher than 3 to set board size: " ))
+        except ValueError:
+            print("You entered incorrect value! Try again.")
+            continue
+        if n > 2 and n % 2 == 1:
+            break
+    Nboard = [[0 for j in range(n)] for i in range(n)]    
+    count = 1
+    for i in range(n):
+        for j in range(n):
+           Nboard[i][j] = str(count)
+           count += 1
+    Nboard[n//2][n//2] = "X"
+
+    
 
 def display_board(board):
-    print(hor_line)
-    for i in range(3):
-        print(ver_lines)
-        print("|  ",board[i][0],div,board[i][1],div,board[i][2],"  |")
-        print(ver_lines)
-        print(hor_line)
+    print(hor_line*n+"+")
+    for i in range(n):
+        print(ver_lines*n+"|")
+        stor =""
+        for j in range(n-1):
+            stor += f'{board[i][j]:{" "}^{9}}' + "|" 
+        print("|" + stor + f'{board[i][n-1]:{" "}^{9}}'+"|")
+        print(ver_lines*n+"|")
+        print(hor_line*n+"+")
     # The function accepts one parameter containing the board's current status
     # and prints it out to the console.
 
@@ -27,11 +45,12 @@ def enter_move(board):
         
         while True: # loop for input validation
             try:
-                player_move = int(input("Enter a number from 1 to 9: " ))
+                print(f"Enter a number from 1 to {n**2}: ") 
+                player_move = int(input())
             except ValueError:
                 print("You entered incorrect value! Try again.")
                 continue
-            if player_move > 0 and player_move < 10:
+            if player_move > 0 and player_move < n**2+1:
                 break
                 
         for item in free_fields: # check if the field entered by player is free
@@ -50,8 +69,8 @@ def enter_move(board):
 def make_list_of_free_fields(board):
     global free_fields
     free_fields = []
-    for row in range(3):
-        for column in range(3):
+    for row in range(n):
+        for column in range(n):
             if board[row][column] == "X" or board[row][column] == "O" :
                 continue
             else:
@@ -61,21 +80,29 @@ def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares; 
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
 
+
+
 def victory_for(board, sign):
     global end_of_game
     make_list_of_free_fields(board)
-    # I don't know how to make code for checking win conditions less bulky :c
-    for i in range(3):
-        if (board[0][i] + board[1][i] + board[2][i] == sign*3) or\
-           (board[i][0] + board[i][1] + board[i][2] == sign*3):    # checking horisontal and vertical lines
+    board_tr = [list(row) for row in zip(*board)]
+    diag_1 = ''
+    diag_2 = ''
+    
+    for i in range(n):
+        diag_1 += board[i][i]
+        diag_2 += board[i][n-1-i]
+        hori = ''.join(board[i])
+        vert = ''.join(board_tr[i])
+        if (hori == sign*n) or (vert == sign*n):    # checking horisontal and vertical lines
             print(sign,"won!")
             end_of_game = True
             return
-    if (board[0][0] + board[1][1] + board[2][2] == sign*3) or \
-       (board[0][2] + board[1][1] + board[2][0] == sign*3):    # checking both diagonals
-           print(sign,"won!")
-           end_of_game = True
-           return
+    
+    if (diag_1 == sign*n) or (diag_2 == sign*n):    # checking both diagonals
+        print(sign,"won!")
+        end_of_game = True
+        return
     if len(free_fields) == 0:
         print("No one won.")
         end_of_game = True
@@ -87,7 +114,7 @@ def victory_for(board, sign):
 def draw_move(board):
     make_list_of_free_fields(board)
     while True: # loop until randomizer chooses a free field
-        comp_move = str(randrange(1,10))
+        comp_move = str(randrange(1,n**2+1))
         for item in free_fields: # checking if the field picked by randomizer is free 
             if board[item[0]][item[1]] == comp_move:
                 board[item[0]][item[1]] = "X"
@@ -96,7 +123,8 @@ def draw_move(board):
     # restart the main loop if the field is already occupied
      
 
-            
+
+generate_board()              
 display_board(Nboard)            
 while end_of_game == False: 
     enter_move(Nboard)
@@ -108,6 +136,7 @@ while end_of_game == False:
     draw_move(Nboard)
     display_board(Nboard)
     victory_for(Nboard,"X")
+
 
 
 
